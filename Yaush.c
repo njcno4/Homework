733b61c3744b5e2e37f24b47d71b4ece4d725d3e
njcno4 <yueh13@mails.tsinghua.edu.cn>
2014-04-16 22:12:35 +0800

    added history functions, to do: history search

--- Yaush.c
+++ Yaush.c
@@ -12,6 +12,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/file.h>
+#include <sys/stat.h>
+#include <sys/errno.h>
+
+#include <readline/readline.h>
+#include <readline/history.h>
 
 static char input = '\0';
 static char ** commandArray;
@@ -55,13 +62,13 @@ void executeCommand(){
 	switch (pid_process){
 	case -1:
 		printf("Fork failure \n Exiting \n");
-		exit();
+		exit(1);
 		break;
 	case 0:
 	 //executer le code
 		break;
 	default:
-		wait();
+		//wait();
 		break;
 	}
 }
@@ -72,23 +79,64 @@ void welcome_msg(){
 	printf("----------------------- \n");
 }
 
+char * test_white (char * line){
+	int length_line = strlen(line);
+	int begining_line_index = 0;
+	int i;
 
-int main(void) {
+	while(line[begining_line_index] == ' '){
+		begining_line_index++;
 
+	}
+
+	if(begining_line_index == length_line)
+		return NULL;
+
+	while(line[length_line - 1] == ' '){
+		length_line --;
+
+		if(length_line == 0)
+			return NULL;
+	}
+	int up_index = (length_line - begining_line_index);
+	char * new_line =  (char*) malloc(up_index * sizeof(char));
+
+
+	for( i = 0; i < up_index; i++){
+		new_line[i] = line[begining_line_index + i];
+	}
+	new_line[up_index] = '\0';
+
+	return new_line;
+}
+int main(int argc, char ** argv) {
+	char *line, *history_tmp;
+	//initialize_readline();
+	using_history ();
 	welcome_msg();
-	nextLine();
+
 
 	while(1){
-		input = getchar();
-		if(input == '\n')
-			nextLine();
-			//Do nothing
-		else {
-			listenCommand(); //Do nothing
-			executeCommand(); //Execute command
-			nextLine(); //Do nothing
+		line = readline("Oui maitre >>>");
+
+		if(!line)
+			break;
+
+		history_tmp = test_white (line);
+		// removes begining and en spaces
+		if(history_tmp != NULL){
+			add_history(history_tmp);
 		}
-		printf("Good Bye");
+
+		if (strcmp (line, "exit") == 0)
+			{printf("Good Bye");
+			return EXIT_SUCCESS;
+			}
+
+		free(line);
+
 	}
-	return EXIT_SUCCESS;
+
 }
+
+
