9e9c8331cc845e63f6b4f7340979f59b00f56667
njcno4 <yueh13@mails.tsinghua.edu.cn>
2014-06-10 21:09:10 +0800

    Last modifications

--- Function_utils.h
+++ Function_utils.h
@@ -39,7 +39,8 @@ void interpret_command (char * line){
 	int state_bg = 1;
 	int i;
 	int j;
-
+	redir_stdout = 0;
+	redir_stdin  = 0;
 	if(line == NULL)
 		return;
 
@@ -66,22 +67,21 @@ void interpret_command (char * line){
 	}
 
 	if (command_vect_index > 1){
+
 		if(strcmp(argv[command_vect_index - 2],">>") == 0){
 			redir_stdout = 1;
 			strcpy(descriptor_name , argv[command_vect_index -1]);
 			command_vect_index = command_vect_index - 2;
 		}
-		else
-			redir_stdout = 0;
 
-		if(strcmp(argv[command_vect_index - 2],"<<") == 0){
-			redir_stdin = 1;
-			strcpy(descriptor_name , argv[command_vect_index -1]);
-			command_vect_index = command_vect_index - 2;
+		if(command_vect_index > 1){
+			if(strcmp(argv[command_vect_index - 2],"<<") == 0){
+				redir_stdin = 1;
+				strcpy(descriptor_name , argv[command_vect_index -1]);
+				command_vect_index = command_vect_index - 2;
+			}
+
 		}
-		else
-			redir_stdin = 0;
-
 		for(i = 0; i < command_vect_index; i++){
 
 			if(strcmp(argv[i],"|") == 0){
@@ -91,10 +91,7 @@ void interpret_command (char * line){
 				argv_1[i] = NULL;
 
 				for(j = 0; j < command_vect_index - i - 1; j++){
-					printf("argv[%d] = %s \n", j + i + 1, argv[j+i+1]);
 					argv_2[j] = argv[j+i+1];
-
-					printf("argv_2[%d] = %s \n", j, argv_2[j]);
 				}
 				argv_2[command_vect_index - i - 1] = NULL;
 
@@ -168,5 +165,136 @@ void welcome_msg(){
 	printf("------------------------------------------------------------------- \n");
 	printf("------------------This is a shell project--------------------------\n");
 	printf("------------------------------------------------------------------- \n");
+
+}
+
+char * test_white (char * line){
+	//Remove white spaces in at the beginning or at the end of the line
+	int length_line = strlen(line);
+	int begining_line_index = 0;
+	int i;
+
+	while(line[begining_line_index] == ' '){
+		begining_line_index++;
+
+	}
+
+	if(begining_line_index == length_line)
+		return NULL;
+
+	while(line[length_line - 1] == ' '){
+		length_line --;
+
+		if(length_line == 0)
+			return NULL;
+	}
+	int up_index = (length_line - begining_line_index);
+	char * new_line =  (char*) malloc((up_index + 1) * sizeof(char));
+
+
+	for( i = 0; i < up_index; i++){
+		new_line[i] = line[begining_line_index + i];
+	}
+	new_line[up_index] = '\0';
+
+	return new_line;
+}
+
+
+int verify_command(char * command){
+	//Verifies if the command is in the command_tab list.
+	// if yes, returns its index, otherwise, returns -1
+	int i;
+	for(i = 0; i < max_i; i++){
+		if(strcmp(command_tab[i].name, command) == 0 )
+			return i;
+	}
+	return -1;
+}
+
+
+void interpret_history_command(char * line){
+	/* Interpret history commands. Should begin with the string hist-.
+	hist-save saves history list in history.txt,
+	hist-read loads history.txt
+	hist-list list history
+	hist-clear clears history list
+	 */
+	if(strcmp(line, "hist-save") == 0){
+		write_history ("history.txt");
+		printf("history saved in \"history.txt\" \n");
+	}
+	else if (strcmp(line, "hist-read") == 0){
+		read_history("history.txt");
+		printf("history read in \"history.txt\" \n");
+	}
+	else if (strcmp (line, "hist-list") == 0)
+	{
+		HIST_ENTRY ** hst_list;
+		int i;
+
+		hst_list = history_list();
+
+		if(hst_list != NULL)
+			for (i = 0; hst_list[i]; i++)
+				printf ("%d: %s\n", i + history_base, hst_list[i]->line);
+	}
+
+	else if (strncmp (line, "hist-clear", 10) == 0)
+	{
+		char answer[1024];
+		printf("Are you sure to clear the history ? [Y/N]\n");
+		scanf("%s", answer);
+		if(strncmp(answer, "Y", 1) == 0){
+			clear_history();
+			write_history ("history.txt");
+		}
+		printf("history cleared \n");
+	}
+	else
+		printf("Unknown history command \n");
+}
+
+int compare( const void * word1, const void * word2) {
+	CMD * w1 = (CMD *) word1;
+	CMD * w2 = (CMD *) word2;
+	return strcmp(w1->name, w2->name);
+}
+
+
+void construct_command_tab(){
+	//prints the name of all the files in the directory in the static "command_tab" and puts it in alphabetical order
+	DIR *dir;
+	struct dirent *entry;
+	char * path_vect;
+	char * path_buf;
+
+	path_vect = getenv("PATH");
+
+	path_buf = strtok (path_vect,":");
+	while (path_buf != NULL)
+	{
+		dir = opendir (path_buf);
+		if(dir != NULL){
+			while((entry = readdir(dir)) != NULL){
+				if (entry->d_type == DT_REG){
+					strcpy(command_tab[max_i].name, entry->d_name);
+					strcpy(command_tab[max_i].path, path_buf);
+					strcat(command_tab[max_i].path, "/");
+					max_i++;
+				}
+			}
+		}
+		else{
+			printf("Error opening /bin \n");
+		}
+		closedir(dir);
+
+
+		path_buf = strtok (NULL, ":");
+	}
+
+
+	qsort (command_tab, max_i, sizeof(CMD), compare);
 
 }
--- Yaush.c
+++ Yaush.c
@@ -26,34 +26,32 @@ void executeCommand_pipe(char * command_path_1, char ** argv_1, char * command_p
 		pid_process_2 = fork();
 		switch (pid_process_2){
 
-			case -1:
-				printf("Fork failure \n Exiting \n");
-				exit(1);
-				break;
+		case -1:
+			printf("Fork failure \n Exiting \n");
+			exit(1);
+			break;
 
-			case 0:
-				dup2(par[0], STDIN_FILENO);
-				close(par[0]);close(par[1]);
-				close(chd[0]); close(chd[1]);
-				execv(command_path_2, argv_2);
-				exit(EXIT_SUCCESS);
-				break;
+		case 0:
+			dup2(par[0], STDIN_FILENO);
+			close(par[0]);close(par[1]);
+			close(chd[0]); close(chd[1]);
+			execv(command_path_2, argv_2);
+			exit(EXIT_SUCCESS);
+			break;
 
-			default:
-				dup2(par[1], STDOUT_FILENO);
-				close(par[0]);close(par[1]);
-				close(chd[0]); close(chd[1]);
-				execv(command_path_1, argv_1);
-				break;
-			}
+		default:
+			dup2(par[1], STDOUT_FILENO);
+			close(par[0]);close(par[1]);
+			close(chd[0]); close(chd[1]);
+			execv(command_path_1, argv_1);
+			break;
+		}
 		exit(EXIT_SUCCESS);
 		break;
 
-	default:
-
-		waitpid(pid_process, &status1, WUNTRACED);
-
-		break;
+		default:
+			waitpid(pid_process, &status1, WUNTRACED);
+			break;
 
 	}
 
@@ -83,17 +81,17 @@ void executeCommand(char * command_path, char ** argv, int state_bg){
 		if(redir_stdout == 1){
 			devNull  = open(descriptor_name, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 			dup2(devNull, STDOUT_FILENO);
-		}else if(redir_stdin == 1){
-			devNull  = open(descriptor_name, O_WRONLY);
+		}
+		if(redir_stdin == 1){
+			devNull  = open(descriptor_name, O_RDONLY, 0600);
 			dup2(devNull, STDIN_FILENO);
 		}
-		else{
 
-			if(!state_bg){
-				devNull = open("/dev/null", O_WRONLY);
-				dup2(devNull, STDOUT_FILENO);
-			}
+		if(!state_bg){
+			devNull = open("/dev/null", O_WRONLY);
+			dup2(devNull, STDOUT_FILENO);
 		}
+
 
 		execv(command_path, argv);
 		exit(EXIT_SUCCESS);
@@ -108,7 +106,6 @@ void executeCommand(char * command_path, char ** argv, int state_bg){
 		else{
 			printf("+ [%d] %s \n", (int) pid_process, command_path);
 		}
-		printf("Exit status: %d \n", status);
 		break;
 
 
@@ -118,136 +115,6 @@ void executeCommand(char * command_path, char ** argv, int state_bg){
 }
 
 
-char * test_white (char * line){
-	//Remove white spaces in at the beginning or at the end of the line
-	int length_line = strlen(line);
-	int begining_line_index = 0;
-	int i;
-
-	while(line[begining_line_index] == ' '){
-		begining_line_index++;
-
-	}
-
-	if(begining_line_index == length_line)
-		return NULL;
-
-	while(line[length_line - 1] == ' '){
-		length_line --;
-
-		if(length_line == 0)
-			return NULL;
-	}
-	int up_index = (length_line - begining_line_index);
-	char * new_line =  (char*) malloc((up_index + 1) * sizeof(char));
-
-
-	for( i = 0; i < up_index; i++){
-		new_line[i] = line[begining_line_index + i];
-	}
-	new_line[up_index] = '\0';
-
-	return new_line;
-}
-
-
-int verify_command(char * command){
-	//Verifies if the command is in the command_tab list.
-	// if yes, returns its index, otherwise, returns -1
-	int i;
-	for(i = 0; i < max_i; i++){
-		if(strcmp(command_tab[i].name, command) == 0 )
-			return i;
-	}
-	return -1;
-}
-
-
-void interpret_history_command(char * line){
-	/* Interpret history commands. Should begin with the string hist-.
-	hist-save saves history list in history.txt,
-	hist-read loads history.txt
-	hist-list list history
-	hist-clear clears history list
-	 */
-	if(strcmp(line, "hist-save") == 0){
-		write_history ("history.txt");
-		printf("history saved in \"history.txt\" \n");
-	}
-	else if (strcmp(line, "hist-read") == 0){
-		read_history("history.txt");
-		printf("history read in \"history.txt\" \n");
-	}
-	else if (strcmp (line, "hist-list") == 0)
-	{
-		HIST_ENTRY ** hst_list;
-		int i;
-
-		hst_list = history_list();
-
-		if(hst_list != NULL)
-			for (i = 0; hst_list[i]; i++)
-				printf ("%d: %s\n", i + history_base, hst_list[i]->line);
-	}
-
-	else if (strncmp (line, "hist-clear", 10) == 0)
-	{
-		char answer[1024];
-		printf("Are you sure to clear the history ? [Y/N]\n");
-		scanf("%s", answer);
-		if(strncmp(answer, "Y", 1) == 0){
-			clear_history();
-			write_history ("history.txt");
-		}
-		printf("history cleared \n");
-	}
-	else
-		printf("Unknown history command \n");
-}
-
-int compare( const void * word1, const void * word2) {
-	CMD * w1 = (CMD *) word1;
-	CMD * w2 = (CMD *) word2;
-	return strcmp(w1->name, w2->name);
-}
-
-
-void construct_command_tab(){
-	//prints the name of all the files in the directory in the static "command_tab" and puts it in alphabetical order
-	DIR *dir;
-	struct dirent *entry;
-	char * path_vect;
-	char * path_buf;
-
-	path_vect = getenv("PATH");
-
-	path_buf = strtok (path_vect,":");
-	while (path_buf != NULL)
-	{
-		dir = opendir (path_buf);
-		if(dir != NULL){
-			while((entry = readdir(dir)) != NULL){
-				if (entry->d_type == DT_REG){
-					strcpy(command_tab[max_i].name, entry->d_name);
-					strcpy(command_tab[max_i].path, path_buf);
-					strcat(command_tab[max_i].path, "/");
-					max_i++;
-				}
-			}
-		}
-		else{
-			printf("Error opening /bin \n");
-		}
-		closedir(dir);
-
-
-		path_buf = strtok (NULL, ":");
-	}
-
-
-	qsort (command_tab, max_i, sizeof(CMD), compare);
-
-}
 
 void initialize_readline(){
 	rl_readline_name = "Yaush";
